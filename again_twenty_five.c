dd567953c767caffa86d8c392d8f9424d6d8bf7a
Yuliang Jin <jinyuliang@umeng.com>
2016-12-29 10:57:37 +0800

    add 630A

---
+++ again_twenty_five.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main (int argc, char * argv[]) {
+    long long n;
+    // scanf ("%I64d", &n);
+    scanf ("%lld", &n);
+
+    printf ("25\n");
+
+    return 0;
+}
+
