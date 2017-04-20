5f4ecadb56ec0a036ee40deffda78a66fc685e2b
Yuliang Jin <jinyuliang@umeng.com>
2017-04-21 01:06:27 +0800

    add 682B

---
+++ alyona_and_mex.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var n int
+	if _, err := fmt.Scanf("%d\n", &n); err != nil {
+		return
+	}
+
+	array := make([]int, n)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for i := 0; i < n && scanner.Scan(); i++ {
+		a, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return
+		}
+		array[i] = a
+	}
+
+	merge_sort(array, 0, n-1)
+
+	current := 1
+	for i := 0; i < n; i++ {
+		if array[i] >= current {
+			array[i] = current
+			current++
+		}
+	}
+	fmt.Println(current)
+}
+
+func merge(buffer []int, start, middle, end int) {
+	sorted := make([]int, end-start+1)
+
+	i := start
+	j := middle + 1
+	k := 0
+	for i <= middle && j <= end {
+		if buffer[i] <= buffer[j] {
+			sorted[k] = buffer[i]
+			i++
+		} else {
+			sorted[k] = buffer[j]
+			j++
+		}
+		k++
+	}
+	for i <= middle {
+		sorted[k] = buffer[i]
+		k++
+		i++
+	}
+	for j <= end {
+		sorted[k] = buffer[j]
+		k++
+		j++
+	}
+
+	i = start
+	k = 0
+	for i <= end {
+		buffer[i] = sorted[k]
+		i++
+		k++
+	}
+}
+
+func merge_sort(buffer []int, start, end int) {
+	if end > start {
+		middle := (end + start) / 2
+		merge_sort(buffer, start, middle)
+		merge_sort(buffer, middle+1, end)
+		merge(buffer, start, middle, end)
+	}
+}
