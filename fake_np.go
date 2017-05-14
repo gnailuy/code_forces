122186bdf1a55d711fd935b28bcbf3c937c88309
Yuliang Jin <jinyuliang@umeng.com>
2017-05-14 11:14:03 +0800

    improve 805A

--- fake_np.go
+++ fake_np.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -11,33 +10,9 @@ func main() {
 		return
 	}
 
-	r_sqrt := int(math.Sqrt(float64(r)))
-	not_prime := make([]bool, r_sqrt+1)
-	for i := 2; i <= r_sqrt; i++ {
-		for j := i; j <= r_sqrt; j++ {
-			if i*j <= r_sqrt {
-				not_prime[i*j] = true
-			} else {
-				break
-			}
-		}
-	}
-	primes := []int{2}
-	for i := 3; i <= r_sqrt; i++ {
-		if !not_prime[i] {
-			primes = append(primes, i)
-		}
-	}
-
-	if l != r || l%2 == 0 {
+	if l != r {
 		fmt.Println(2)
 	} else {
-		for i := 0; i < len(primes); i++ {
-			if l%primes[i] == 0 {
-				fmt.Println(primes[i])
-				return
-			}
-		}
 		fmt.Println(l)
 	}
 }
