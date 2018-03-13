2194bc79d5341d53045a2e25e540f6311d827d15
Yuliang Jin <sheldon.y.jin@gmail.com>
2018-03-13 15:17:42 +0800

    improve Leet 1

--- two_sum.go
+++ two_sum.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
+	num_map := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+		pair := target - nums[i]
+		if j, ok := num_map[pair]; ok {
+			return []int{j, i}
+		} else {
+			num_map[nums[i]] = i
 		}
 	}
 	return nil
