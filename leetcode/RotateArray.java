4d7999f7043367c6b68e2e31498731e8a47892ce
Yuliang Jin <jyl198803@gmail.com>
2018-04-26 21:16:38 +0800

    add LeetCode

--- leetcode/RotateArray.java
+++ leetcode/RotateArray.java
@@ -9,6 +9,45 @@ class RotateArray {
         }
     }
 
+    public int gcd(int x, int y) {
+        int tmp;
+        if (x < y) {
+            tmp = x;
+            x = y;
+            y = tmp;
+        }
+        while (true) {
+            if (x % y == 0) {
+                return y;
+            } else {
+                tmp = x % y;
+                x = y;
+                y = tmp;
+            }
+        }
+    }
+
+    public void rotate_o1(int[] nums, int k) {
+        if (nums.length <= 1) {
+            return;
+        }
+        k = k%nums.length;
+        if (k == 0) {
+            return;
+        }
+
+        int unit = gcd(nums.length, k);
+        for (int idx = 0; idx < unit; idx++) {
+            int extra = nums[idx];
+            for (int i = 0; i < nums.length/unit; i++) {
+                nums[(idx+k)%nums.length] ^= extra;
+                extra ^= nums[(idx+k)%nums.length];
+                nums[(idx+k)%nums.length] ^= extra;
+                idx = (idx+k)%nums.length;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         RotateArray ra = new RotateArray();
 
@@ -23,12 +62,34 @@ class RotateArray {
             int[] nums = test[0];
             int k = test[1][0];
 
-            System.out.println(nums.length);
+            System.out.println(nums.length + " " + k);
             for (int i = 0; i < nums.length; i++) {
                 System.out.print(nums[i] + " ");
             }
             System.out.println();
             ra.rotate(nums, k);
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            System.out.println();
+            System.out.println();
+        }
+
+        int[][][] tests_o1 = {
+            {{1}, {1}},
+            {{1, 2, 3, 4, 5, 6}, {2}},
+            {{1, 2, 3, 4, 5, 6, 7}, {3}}
+        };
+        for (int[][] test : tests_o1) {
+            int[] nums = test[0];
+            int k = test[1][0];
+
+            System.out.println(nums.length + " " + k);
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            System.out.println();
+            ra.rotate_o1(nums, k);
             for (int i = 0; i < nums.length; i++) {
                 System.out.print(nums[i] + " ");
             }
