be0f319851ae2f89feccf962241315a23da54652
Yuliang Jin <jyl198803@gmail.com>
2018-08-10 22:11:36 +0800

    add LeetCode

---
+++ leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/108/trees-and-graphs/786/
+
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class BinaryTreeInorderTraversal {
+    public void inorderHelper(TreeNode root, List<Integer> out) {
+        if (null == root) return;
+
+        if (null != root.left) {
+            inorderHelper(root.left, out);
+        }
+        out.add(root.val);
+        if (null != root.right) {
+            inorderHelper(root.right, out);
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        inorderHelper(root, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeInorderTraversal btit = new BinaryTreeInorderTraversal();
+
+        int[][] tests = {
+            {1, -1, 2, 3},
+        };
+        for (int[] test : tests) {
+            // TODO: Build Tree
+            // TODO: Inorder Traversal
+        }
+    }
+}
